858a3643d7a66fdb5c129b6c05d81a19afdef55a
Cleine <cleine.sc@gmail.com>
2021-05-23 13:27:34 +0200

    Desktop design + v1 js script

---
+++ fementor/faq-accordion-card/script.js
@@ -0,0 +1,71 @@
+const toggle1 = function() {
+    
+    const elem = document.getElementById("reponse1")
+    if(elem.style.display == "none"){
+        elem.style.display = "block"
+    } else {
+        elem.style.display = "none"
+    }
+}
+
+const toggle2 = function() {
+    
+    const elem = document.getElementById("reponse2")
+    if(elem.style.display == "none"){
+        elem.style.display = "block"
+    } else {
+        elem.style.display = "none"
+    }
+}
+
+const toggle3 = function() {
+    
+    const elem = document.getElementById("reponse3")
+    if(elem.style.display == "none"){
+        elem.style.display = "block"
+    } else {
+        elem.style.display = "none"
+    }
+}
+
+const toggle4 = function() {
+    
+    const elem = document.getElementById("reponse4")
+    if(elem.style.display == "none"){
+        elem.style.display = "block"
+    } else {
+        elem.style.display = "none"
+    }
+}
+
+const toggle5 = function() {
+    
+    const elem = document.getElementById("reponse5")
+    if(elem.style.display == "none"){
+        elem.style.display = "block"
+    } else {
+        elem.style.display = "none"
+    }
+}
+
+
+/*
+function toggle(event) {
+
+    if (event.target && event.target.className == 'question-container') {
+
+        var next = event.target.nextElementSibling;
+        console.log('passage ici')
+
+
+        if (next.style.display == "none") {
+            next.style.display = "block";
+
+        } else {
+            next.style.display = "none";
+        }
+    }
+}
+
+document.addEventListener('click', toggle, true);
+*/
